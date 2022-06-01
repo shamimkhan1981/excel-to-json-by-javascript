58326469b7512f0c2ed8c5a00fbdb376452fb2eb
shamimkhan1981 <shamimkhan1981@gmail.com>
2022-06-01 12:23:59 +0530

    Button added Save converted json data to local drive

--- excel.js
+++ excel.js
@@ -1,30 +1,47 @@
 let selectedFile;
 console.log(window.XLSX);
+document.getElementById("saveJson").style.display = "none";
 document.getElementById('input').addEventListener("change", (event) => {
     selectedFile = event.target.files[0];
 })
 
-let data=[{
-    "name":"jayanth",
-    "data":"scd",
-    "abc":"sdef"
+let data = [{
+    "name": "jayanth",
+    "data": "scd",
+    "abc": "sdef"
 }]
 
-
+let rowObject
 document.getElementById('button').addEventListener("click", () => {
     XLSX.utils.json_to_sheet(data, 'out.xlsx');
-    if(selectedFile){
+    if (selectedFile) {
         let fileReader = new FileReader();
         fileReader.readAsBinaryString(selectedFile);
-        fileReader.onload = (event)=>{
-         let data = event.target.result;
-         let workbook = XLSX.read(data,{type:"binary"});
-         console.log(workbook);
-         workbook.SheetNames.forEach(sheet => {
-              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-              console.log(rowObject);
-              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
-         });
+        fileReader.onload = (event) => {
+            let data = event.target.result;
+            let workbook = XLSX.read(data, { type: "binary" });
+            console.log(workbook);
+            workbook.SheetNames.forEach(sheet => {
+                rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+                console.log(rowObject);
+                document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject, undefined, 4)
+                if (rowObject && typeof rowObject == "object" && rowObject.length > 0) {
+                    document.getElementById("saveJson").style.display = "";
+                } else {
+                    document.getElementById("saveJson").style.display = "none";
+                }
+            });
         }
     }
 });
+
+function saveFile() {
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(new Blob([JSON.stringify(rowObject, null, 2)], {
+        type: "text/plain"
+    }));
+    a.setAttribute("download", "data.json");
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+};
